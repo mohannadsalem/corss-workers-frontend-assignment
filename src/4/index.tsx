574742286d18de677d45240d250fb580b4b07bfe
mohannadsalem <mohannad.salem93@gmail.com>
2023-07-18 16:47:57 +0300

    Feat: Done With Task 4

--- src/4/index.tsx
+++ src/4/index.tsx
@@ -8,25 +8,30 @@ const Task5: FunctionComponent = () => {
 
   const onSubmit = () => {
     // If you want to do something with form submit
-
     alert(`Email: ${email} \nPassword: ${password}`);
   };
 
   return (
     <div id="task-5">
       <form onSubmit={onSubmit}>
-        <label>Email</label>
-        <input
-          name="email"
-          onChange={(event) => setEmail(event.currentTarget.value)}
-          value={email}
-        />
-        <label>Password</label>
-        <input
-          name="password"
-          onChange={(event) => setPassword(event.currentTarget.value)}
-          value={password}
-        />
+        <div className="input-group">
+          <label>Email</label>
+          <input
+            name="email"
+            type="email"
+            onChange={(event) => setEmail(event.currentTarget.value)}
+            value={email}
+          />
+        </div>
+        <div className="input-group">
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            onChange={(event) => setPassword(event.currentTarget.value)}
+            value={password}
+          />
+        </div>
         <button>Login</button>
       </form>
     </div>
